f8904ef871566984e95ec9fc9ae9494929691527
Moderator11 <p7925221@gmail.com>
2025-11-21 00:59:34 +0900

    fix: isOn props renaming

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -4,16 +4,16 @@ import styled from "styled-components";
 export function ToggleButton() {
   const [isOn, setIsOn] = useState(false);
 
-  return <Switch isOn={isOn} onClick={() => setIsOn(!isOn)} />;
+  return <Switch $isOn={isOn} onClick={() => setIsOn(!isOn)} />;
 }
 
-const Switch = styled.button<{ isOn: boolean }>`
+const Switch = styled.button<{ $isOn: boolean }>`
   width: 60px;
   height: 30px;
   border-radius: 15px;
   border: none;
   cursor: pointer;
-  background-color: ${({ isOn }) => (isOn ? "green" : "gray")};
+  background-color: ${({ $isOn }) => ($isOn ? "green" : "gray")};
   position: relative;
   transition: background-color 0.3s;
 
@@ -21,7 +21,7 @@ const Switch = styled.button<{ isOn: boolean }>`
     content: "";
     position: absolute;
     top: 3px;
-    left: ${({ isOn }) => (isOn ? "32px" : "3px")};
+    left: ${({ $isOn }) => ($isOn ? "32px" : "3px")};
     width: 24px;
     height: 24px;
     background: white;
