8daca6c45fd7965d200ce6b7c68c9fe0aed96638
Moderator11 <p7925221@gmail.com>
2025-11-29 00:47:44 +0900

    fix: protected route user set to null when me fail

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -2,14 +2,17 @@ import { useContext, useEffect, type ReactNode } from "react";
 import { AuthContext } from "src/contexts/AuthContext";
 import { toast } from "react-toastify";
 import { useNavigateBack } from "@hooks/userNavigateBack";
+import { useMe } from "@apis/hooks/useMe";
 
 export default function ProtectedRoute({ element }: { element: ReactNode }) {
-  const { user, loading } = useContext(AuthContext);
+  const { user, setUser, loading } = useContext(AuthContext);
   const navigateBack = useNavigateBack();
+  const { isError } = useMe();
 
   useEffect(() => {
-    if (!loading && !user) {
+    if ((!loading && !user) || isError) {
       toast.info("Require login to see this page");
+      setUser(null);
       navigateBack();
     }
   }, [user, loading, navigateBack]);
